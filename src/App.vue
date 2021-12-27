4ae63f5b70468a04c1ebe1220af7269ee6af27db
asakuchi <paleApricot.asakuchi@gmail.com>
2021-12-27 18:28:15 +0900

    add custom component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Mybutton />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Mybutton from './components/MyButton.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    Mybutton,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/MyButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button class="color">My Button</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped lang="scss">
+  .color {
+    color: blue;
+    background-color: aquamarine;
+  }
+</style>
